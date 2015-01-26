ad8c2fa65a3fe4ca093a89ffb248edd36e740b2c
Tim Martin <timothy.m.martin@gmail.com>
2015-01-25 16:22:00 -0800

    (#58) Add signup controller.

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -2,5 +2,45 @@
 
 angular.module('badgerApp')
   .controller('SignupCtrl', function ($scope) {
-    $scope.message = 'Hello';
+    $scope.user = {};
+    $scope.errors = {};
+
+    $scope.accountOptions = [ 'Client', 'Worker' ];
+    $scope.user.accountType = $scope.accountOptions[0];
+
+    $scope.workerQuestions = [
+    	{ field: 'skills', text: 'What are your skills/professions?' },
+    	{ field: 'rate', text: 'What is your hourly rate?' },
+    	{ field: 'advert', text: 'Anything else you would like to say about your work?' }
+    ];
+
+    $scope.register = function(form) {
+      $scope.submitted = true;
+
+      if(form.$valid) {
+      	var newUser = {
+          name: $scope.user.name,
+          email: $scope.user.email,
+          password: $scope.user.password,
+          accountType: $scope.user.accountType
+        };
+
+        // Add worker data if user is creating a worker account.
+        if ($scope.user.accountType === 'Worker') {
+        	newUser.skills = $('#skills'),
+        	newUser.rate = $('#rate'),
+        	newUser.advert = $('#advert')
+        }
+
+        Auth.createUser(newUser)
+        .then( function() {
+          // Account created, redirect to home.
+          $location.path('/');
+        })
+        .catch( function(err) {
+          $scope.errors.other = err.message;
+        });
+      }
+    };
+
   });
