1ba1c66b5b7804a4d6cbf00aebc2e826f5b7f4a5
Tim Martin <timothy.m.martin@gmail.com>
2015-01-26 11:41:19 -0800

    (#58) Fix issue where  wans't passed into signup.conroller.spec.

--- client/app/account/signup/signup.controller.spec.js
+++ client/app/account/signup/signup.controller.spec.js
@@ -8,18 +8,18 @@ describe('Controller: SignupCtrl', function () {
   var SignupCtrl, scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $scope) {
     scope = $rootScope.$new();
     SignupCtrl = $controller('SignupCtrl', {
       $scope: scope
     });
   }));
 
-  it('should default to account type client', function () {
+  it('should default to account type client', function ($scope) {
     expect($scope.user.accountType).toEqual('Client');
   });
 
-  it('should display an error if email is left blank', function () {
+  it('should display an error if email is left blank', function ($scope) {
     $scope.register();
     expect($('.help-block')).toEqual(true);
   });
