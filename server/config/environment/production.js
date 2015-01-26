f708ca82f294fefc9fc564aaf6b5d4cd6b7ccf22
Scott Rice <scottrice10@gmail.com>
2015-01-26 15:39:11 -0800

    (#22) Change name of production database

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -7,7 +7,7 @@ exports = {
   // Create a database connection and export it from this file.
   // Connecting with the user "root", no password,
   // and to the database "ihammer".
-  mysql: new Sequelize('ihammer', 'root', '', {
+  mysql: new Sequelize('ihammer_prod', 'root', '', {
     host: process.env.DATABASE_HOST,
     port: '3306',
     dialect: 'mysql'
