baa90c631f07f4824897c76f156dfc9d98555307
Tim Martin <timothy.m.martin@gmail.com>
2015-01-25 17:25:48 -0800

    (#69) Add login and signup links to navbar.

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -8,6 +8,14 @@ angular.module('badgerApp')
     }];
 
     $scope.isCollapsed = true;
+    // $scope.isLoggedIn = Auth.isLoggedIn;
+    // $scope.isAdmin = Auth.isAdmin;
+    // $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
 
     $scope.isActive = function(route) {
       return route === $location.path();
