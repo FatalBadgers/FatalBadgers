f45a4437d6b1a7128595d5ba7f0e857796ea8baf
Tim Martin <timothy.m.martin@gmail.com>
2015-01-26 14:31:14 -0800

    (#58) Update for signup tests.

--- client/app/account/signup/signup.controller.spec.js
+++ client/app/account/signup/signup.controller.spec.js
@@ -1,28 +1,35 @@
 'use strict';
 
 describe('Controller: SignupCtrl', function () {
+  var SignupCtrl, createController, $scope, $rootScope, $controller, $form;
 
-  // load the controller's module
+  // Load the controller's module
   beforeEach(module('badgerApp'));
 
-  var SignupCtrl, scope;
-
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    SignupCtrl = $controller('SignupCtrl', {
-      $scope: scope
-    });
+  beforeEach(inject(function ($injector) {
+    $rootScope = $injector.get('$rootScope');
+    $controller = $injector.get('$controller');
+
+    $scope = $rootScope.$new();
+
+    createController = function() {
+      SignupCtrl = $controller('SignupCtrl', {
+        $scope: $scope
+      });
+    };
   }));
 
-  it('should default to account type client', function ($scope) {
-    console.log($scope);
+  it('should default to account type client', function () {
+    createController();
     expect($scope.user.accountType).toEqual('Client');
   });
 
-  it('should display an error if email is left blank', function ($scope) {
-    $scope.register();
-    expect($('.help-block')).toEqual(true);
+  it('should display an error if email is left blank', function () {
+    createController();
+    $form = $("<form />");
+    $form.trigger("submit");
+    expect(!!$('.help-block')).toEqual(true);
   });
   
 });
