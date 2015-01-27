eae0f233c60d3d703629b094393dbad87c13cf8d
Scott Rice <scottrice10@gmail.com>
2015-01-27 12:22:46 -0800

    (#22) Reverting dev ports to how they were before.

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: 3000
+        port: 9000
       },
       dev: {
         options: {
@@ -190,7 +190,7 @@ module.exports = function (grunt) {
         options: {
           nodeArgs: ['--debug-brk'],
           env: {
-            PORT: 3000
+            PORT: 9000
           },
           callback: function (nodemon) {
             nodemon.on('log', function (event) {
@@ -200,7 +200,7 @@ module.exports = function (grunt) {
             // opens browser on initial server start
             nodemon.on('config:update', function () {
               setTimeout(function () {
-                require('open')('http://localhost:3000/debug?port=5858');
+                require('open')('http://localhost:8080/debug?port=5858');
               }, 500);
             });
           }
