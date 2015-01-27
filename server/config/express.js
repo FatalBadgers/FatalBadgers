2753f8c265b30f3f1ae6e572c8120864819df61a
Scott Rice <scottrice10@gmail.com>
2015-01-26 17:03:49 -0800

    (#22) Staging should not use connect-livereload.

--- server/config/express.js
+++ server/config/express.js
@@ -34,6 +34,14 @@ module.exports = function(app) {
     app.use(morgan('dev'));
   }
 
+  if ('staging' === env) {
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', 'client');
+    app.use(morgan('dev'));
+    app.use(errorHandler()); // Error handler - has to be last
+  }
+
   if ('development' === env || 'test' === env || 'staging' === env) {
     app.use(require('connect-livereload')());
     app.use(express.static(path.join(config.root, '.tmp')));
