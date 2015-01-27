7b5f75fd45d66d49863e0b5dcb959ff2c7364839
Scott Rice <scottrice10@gmail.com>
2015-01-26 16:55:50 -0800

    (#22) Update username and password for OpsWorks controlled databases.

--- server/config/environment/staging.js
+++ server/config/environment/staging.js
@@ -5,7 +5,7 @@ exports = {
   // Create a database connection and export it from this file.
   // Connecting with the user "root", no password,
   // and to the database "ihammer".
-  mysql: new Sequelize('ihammer', 'root', '', {
+  mysql: new Sequelize('ihammer', 'FatalBadgers', 'FatalBadgers', {
     host: process.env.DATABASE_HOST,
     port: '3306',
     dialect: 'mysql'
