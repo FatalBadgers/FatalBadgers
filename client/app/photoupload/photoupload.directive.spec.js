6eee82786064f1567ee4c7e6ecf806db860270b8
Scott Rice <scottrice10@gmail.com>
2015-01-28 18:47:08 -0800

    (#29) Remove second beforeEach in photo upload test.

--- client/app/photoupload/photoupload.directive.spec.js
+++ client/app/photoupload/photoupload.directive.spec.js
@@ -20,7 +20,7 @@ describe('Directive: photoupload', function() {
     });
     $httpBackend.flush();
 
-    createDirective = function(){
+    createDirective = function() {
       // Compile a piece of HTML containing the directive
       $compile("<photoupload></photoupload>")($rootScope);
       // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
@@ -104,67 +104,98 @@ describe('Directive: photoupload', function() {
     $httpBackend.flush();
   });
 
-  describe('Progress and Abort', function() {
-    beforeEach(function() {
-      var file1 = new Blob(['<content>'], {
-        type: 'image/jpeg'
-      });
-      file1.name = 'abc.jpeg';
-
-      var file2 = new Blob(['<content>'], {
-        type: 'image/bmp'
-      });
-      file2.name = 'xyz.bmp';
-
-      var files = [file1, file2];
-
-      createDirective();
-
-      files.forEach(function(file) {
-        $httpBackend.expectGET('/api/s3Policy?mimeType=' + file.type)
-          .respond({
-            s3Policy: 'base64Policy',
-            s3Signature: 'signature',
-            AWSAccessKeyId: 'config.accessKeyId'
-          });
-      });
-
-      $rootScope.onFileSelect(files);
-
-      files.forEach(function(file) {
-        $httpBackend.expectPOST('https://ihammer-dev.s3.amazonaws.com/')
-          .respond(201,
-          '<?xml version="1.0" encoding="UTF-8"?>' +
-          '<PostResponse>' +
-          '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
-          '<Bucket>ihammer-dev</Bucket>' +
-          '<Key>clubs/images/' + file.name + '</Key>' +
-          '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
-          '</PostResponse>');
-      });
-
+  it('should update the progress bar', function() {
+    var file1 = new Blob(['<content>'], {
+      type: 'image/jpeg'
     });
-    it('should update the progress bar', function() {
+    file1.name = 'abc.jpeg';
 
-      expect($rootScope.files[0].progress).toBe(0);
-      expect($rootScope.files[1].progress).toBe(0);
+    var file2 = new Blob(['<content>'], {
+      type: 'image/bmp'
+    });
+    file2.name = 'xyz.bmp';
 
-      $httpBackend.flush();
+    var files = [file1, file2];
 
-      expect($rootScope.files[0].progress).toBe(100);
-      expect($rootScope.files[1].progress).toBe(100);
+    createDirective();
+
+    files.forEach(function(file) {
+      $httpBackend.expectGET('/api/s3Policy?mimeType=' + file.type)
+        .respond({
+          s3Policy: 'base64Policy',
+          s3Signature: 'signature',
+          AWSAccessKeyId: 'config.accessKeyId'
+        });
     });
 
-    it('should allow aborting existing downloads', function() {
-      expect($rootScope.abort).toBeDefined();
+    $rootScope.onFileSelect(files);
 
-      $httpBackend.flush();
-
-      expect($rootScope.upload.length).toBe(2);
-      expect($rootScope.upload[0].abort).toBeDefined();
-      expect($rootScope.upload[1].abort).toBeDefined();
+    files.forEach(function(file) {
+      $httpBackend.expectPOST('https://ihammer-dev.s3.amazonaws.com/')
+        .respond(201,
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<PostResponse>' +
+        '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
+        '<Bucket>ihammer-dev</Bucket>' +
+        '<Key>clubs/images/' + file.name + '</Key>' +
+        '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
+        '</PostResponse>');
     });
 
+    expect($rootScope.files[0].progress).toBe(0);
+    expect($rootScope.files[1].progress).toBe(0);
+
+    $httpBackend.flush();
+
+    expect($rootScope.files[0].progress).toBe(100);
+    expect($rootScope.files[1].progress).toBe(100);
+  });
+
+  it('should allow aborting existing downloads', function() {
+    var file1 = new Blob(['<content>'], {
+      type: 'image/jpeg'
+    });
+    file1.name = 'abc.jpeg';
+
+    var file2 = new Blob(['<content>'], {
+      type: 'image/bmp'
+    });
+    file2.name = 'xyz.bmp';
+
+    var files = [file1, file2];
+
+    createDirective();
+
+    files.forEach(function(file) {
+      $httpBackend.expectGET('/api/s3Policy?mimeType=' + file.type)
+        .respond({
+          s3Policy: 'base64Policy',
+          s3Signature: 'signature',
+          AWSAccessKeyId: 'config.accessKeyId'
+        });
+    });
+
+    $rootScope.onFileSelect(files);
+
+    files.forEach(function(file) {
+      $httpBackend.expectPOST('https://ihammer-dev.s3.amazonaws.com/')
+        .respond(201,
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<PostResponse>' +
+        '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
+        '<Bucket>ihammer-dev</Bucket>' +
+        '<Key>clubs/images/' + file.name + '</Key>' +
+        '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
+        '</PostResponse>');
+    });
+
+    expect($rootScope.abort).toBeDefined();
+
+    $httpBackend.flush();
+
+    expect($rootScope.upload.length).toBe(2);
+    expect($rootScope.upload[0].abort).toBeDefined();
+    expect($rootScope.upload[1].abort).toBeDefined();
   });
 
 });
