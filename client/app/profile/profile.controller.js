17043b32ccf2de31f6f976255f1e95ecd2098df7
Tim Martin <timothy.m.martin@gmail.com>
2015-01-28 16:40:08 -0800

    (#4) Add project history sample data and view.

--- client/app/profile/profile.controller.js
+++ client/app/profile/profile.controller.js
@@ -21,12 +21,14 @@ angular.module('badgerApp')
         title: 'Exmple Project 1',
         summary: 'This is an example project where things were done and stuff.',
         stars: 4,
-        review: 'Good work for a good price.'
+        review: 'Good work for a good price.',
+        client: 'Test Client'
       }, {
         title: 'Exmple Project 2',
         summary: 'This is another, different, example project where different things were done and stuff.',
         stars: 5,
-        review: 'OMG Totes like the best evr.'
+        review: 'OMG Totes like the best evr.',
+        client: 'Client Test'
       }, ],
       advert: "I'm okay. I sleep all night and I work all day."
     };
