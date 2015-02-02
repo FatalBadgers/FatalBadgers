7848de1cfe51d99c37c1ecb470e48de78cfa5280
Scott Rice <scottrice10@gmail.com>
2015-02-02 15:51:55 -0800

    Commenting out slow test.

--- client/app/photoupload/photoupload.directive.spec.js
+++ client/app/photoupload/photoupload.directive.spec.js
@@ -31,31 +31,31 @@ describe('Directive: photoupload', function() {
   });
 
   it('should get Policy for one file', function() {
-    var file = new Blob(['<content>'], {
-      type: 'image/jpeg'
-    });
-    file.name = 'abc.jpeg';
-    var files = [file];
-    $httpBackend.whenGET('/api/s3Policy?mimeType=' + files[0].type)
-      .respond({
-        s3Policy: 'base64Policy',
-        s3Signature: 'signature',
-        AWSAccessKeyId: 'config.accessKeyId'
-      });
-
-    $rootScope.onFileSelect(files);
-
-    $httpBackend.whenPOST('https://ihammer-dev.s3.amazonaws.com/')
-      .respond(201,
-      '<?xml version="1.0" encoding="UTF-8"?>' +
-      '<PostResponse>' +
-      '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
-      '<Bucket>ihammer-dev</Bucket>' +
-      '<Key>clubs/images/pic copy 3.jpg</Key>' +
-      '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
-      '</PostResponse>'
-    );
-    $httpBackend.flush();
+    //var file = new Blob(['<content>'], {
+    //  type: 'image/jpeg'
+    //});
+    //file.name = 'abc.jpeg';
+    //var files = [file];
+    //$httpBackend.whenGET('/api/s3Policy?mimeType=' + files[0].type)
+    //  .respond({
+    //    s3Policy: 'base64Policy',
+    //    s3Signature: 'signature',
+    //    AWSAccessKeyId: 'config.accessKeyId'
+    //  });
+    //
+    //$rootScope.onFileSelect(files);
+    //
+    //$httpBackend.whenPOST('https://ihammer-dev.s3.amazonaws.com/')
+    //  .respond(201,
+    //  '<?xml version="1.0" encoding="UTF-8"?>' +
+    //  '<PostResponse>' +
+    //  '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
+    //  '<Bucket>ihammer-dev</Bucket>' +
+    //  '<Key>clubs/images/pic copy 3.jpg</Key>' +
+    //  '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
+    //  '</PostResponse>'
+    //);
+    //$httpBackend.flush();
   });
 
 });
