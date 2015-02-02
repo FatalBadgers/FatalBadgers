7d9bd0761c8b0d18e8018707a6d8b82209f9e0c0
Tim Martin <timothy.m.martin@gmail.com>
2015-02-02 14:07:37 -0800

    Move logic to controller, reduce redundancy in HTML, add css for link hover.

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -2,7 +2,42 @@
 
 angular.module('badgerApp')
   .controller('DashboardCtrl', function($scope, Auth) {
+
     Auth.getCurrentUser().then(function(user) {
       $scope.currentUser = user;
+      $scope.commonLinks = links[$scope.currentUser.account_type];
     });
+
+    var links = {
+      // Link and text to display for clients.
+      Client: [{
+        title: 'Create A Contract',
+        href: '/create-contract'
+      }, {
+        title: 'Ended Contracts',
+        href: '/end-contract'
+      }, {
+        title: 'Manage Contracts',
+        href: '/manage-contracts'
+      }, {
+        title: 'Search Workers',
+        href: '/worker/search'
+      }],
+
+      // Link and text to display for workers.
+      Worker: [{
+        title: 'Current Contracts',
+        href: '/current-contracts'
+      }, {
+        title: 'Past Contracts',
+        href: '/past-contracts'
+      }, {
+        title: 'Applications',
+        href: '/applications'
+      }, {
+        title: 'Search Contracts',
+        href: '/contract/search'
+      }]
+    };
+
   });
