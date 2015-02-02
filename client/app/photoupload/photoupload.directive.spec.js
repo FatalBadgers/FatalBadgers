2781de65db18f4c6ea6275943baa8471832debb0
Scott Rice <scottrice10@gmail.com>
2015-02-02 14:20:46 -0800

    Use when instead of expect with $httpbackend.

--- client/app/photoupload/photoupload.directive.spec.js
+++ client/app/photoupload/photoupload.directive.spec.js
@@ -18,10 +18,6 @@ describe('Directive: photoupload', function() {
         bucket: 'ihammer-dev'
       }
     });
-    // check that http requests are getting handled correctly
-    $httpBackend.whenGET('/api/config').respond(200);
-    $httpBackend.whenGET('app/main/main.html').respond(200);
-    $httpBackend.whenPOST('/api/user/getuser').respond(302);
     $httpBackend.flush();
 
     // Compile a piece of HTML containing the directive
@@ -60,6 +56,134 @@ describe('Directive: photoupload', function() {
       '</PostResponse>'
     );
     $httpBackend.flush();
+  });
+
+  it('should get Policy for multiple files', function() {
+    var file1 = new Blob(['<content>'], {
+      type: 'image/jpeg'
+    });
+    file1.name = 'abc.jpeg';
+
+    var file2 = new Blob(['<content>'], {
+      type: 'image/bmp'
+    });
+    file2.name = 'xyz.bmp';
+
+    var files = [file1, file2];
+
+    files.forEach(function(file) {
+      $httpBackend.whenGET('/api/s3Policy?mimeType=' + file.type)
+        .respond({
+          s3Policy: 'base64Policy',
+          s3Signature: 'signature',
+          AWSAccessKeyId: 'config.accessKeyId'
+        });
+    });
+
+    $rootScope.onFileSelect(files);
+
+    files.forEach(function(file) {
+      $httpBackend.whenPOST('https://ihammer-dev.s3.amazonaws.com/')
+        .respond(201,
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<PostResponse>' +
+        '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
+        '<Bucket>ihammer-dev</Bucket>' +
+        '<Key>clubs/images/' + file.name + '</Key>' +
+        '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
+        '</PostResponse>');
+    });
+
+    $httpBackend.flush();
+  });
+
+  it('should update the progress bar', function() {
+    var file1 = new Blob(['<content>'], {
+      type: 'image/jpeg'
+    });
+    file1.name = 'abc.jpeg';
+
+    var file2 = new Blob(['<content>'], {
+      type: 'image/bmp'
+    });
+    file2.name = 'xyz.bmp';
+
+    var files = [file1, file2];
+
+    files.forEach(function(file) {
+      $httpBackend.whenGET('/api/s3Policy?mimeType=' + file.type)
+        .respond({
+          s3Policy: 'base64Policy',
+          s3Signature: 'signature',
+          AWSAccessKeyId: 'config.accessKeyId'
+        });
+    });
+
+    $rootScope.onFileSelect(files);
+
+    files.forEach(function(file) {
+      $httpBackend.whenPOST('https://ihammer-dev.s3.amazonaws.com/')
+        .respond(201,
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<PostResponse>' +
+        '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
+        '<Bucket>ihammer-dev</Bucket>' +
+        '<Key>clubs/images/' + file.name + '</Key>' +
+        '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
+        '</PostResponse>');
+    });
+
+    expect($rootScope.files[0].progress).toBe(0);
+    expect($rootScope.files[1].progress).toBe(0);
+
+    $httpBackend.flush();
+
+    expect($rootScope.files[0].progress).toBe(100);
+    expect($rootScope.files[1].progress).toBe(100);
+  });
+
+  it('should allow aborting existing downloads', function() {
+    var file1 = new Blob(['<content>'], {
+      type: 'image/jpeg'
+    });
+    file1.name = 'abc.jpeg';
+
+    var file2 = new Blob(['<content>'], {
+      type: 'image/bmp'
+    });
+    file2.name = 'xyz.bmp';
+
+    var files = [file1, file2];
+    files.forEach(function(file) {
+      $httpBackend.whenGET('/api/s3Policy?mimeType=' + file.type)
+        .respond({
+          s3Policy: 'base64Policy',
+          s3Signature: 'signature',
+          AWSAccessKeyId: 'config.accessKeyId'
+        });
+    });
+
+    $rootScope.onFileSelect(files);
+
+    files.forEach(function(file) {
+      $httpBackend.whenPOST('https://ihammer-dev.s3.amazonaws.com/')
+        .respond(201,
+        '<?xml version="1.0" encoding="UTF-8"?>' +
+        '<PostResponse>' +
+        '<Location>https://ihammer-dev.s3.amazonaws.com/clubs%2Fimages%2Fpic+copy+3.jpg</Location>' +
+        '<Bucket>ihammer-dev</Bucket>' +
+        '<Key>clubs/images/' + file.name + '</Key>' +
+        '<ETag>"971d696d8ef20fbe43387c817f83dc52"</ETag>' +
+        '</PostResponse>');
+    });
+
+    expect($rootScope.abort).toBeDefined();
+
+    $httpBackend.flush();
+
+    expect($rootScope.upload.length).toBe(2);
+    expect($rootScope.upload[0].abort).toBeDefined();
+    expect($rootScope.upload[1].abort).toBeDefined();
   });
 
 });
